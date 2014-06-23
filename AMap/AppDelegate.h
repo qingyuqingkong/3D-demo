32a4e7dfa770ccac063aa35cfa2c34a0a279ead3
潘强 <panqiang@panqiangdeMac-mini.local>
2014-06-23 10:09:57 +0800

    Initial Commit

---
+++ AMap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AMap
+//
+//  Created by 潘强 on 14-6-23.
+//  Copyright (c) 2014年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AMap
+//
+//  Created by 潘强 on 14-6-23.
+//  Copyright (c) 2014年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
